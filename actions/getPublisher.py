9bb2d98e0eec4006387f5a2c8e985188fb45947b
starbase <mhorn71@gmail.com>
2014-04-28 00:40:44 +0100

    Added getPublisher action.

--- actions/getPublisher.py
+++ actions/getPublisher.py
@@ -1,4 +1,3 @@
-__author__ = 'mark'
 import utilities.samplerstatus as samplerstatus
 import ConfigParser
 import logging
@@ -7,7 +6,29 @@ import logging
 logger = logging.getLogger('actions.getPublisher')
 
 config = ConfigParser.RawConfigParser()
-config.read("StarinetBeagleLogger.conf")
 
-def control(buffer0):
-    return 0, None
+
+def control():
+    config.read("StarinetBeagleLogger.conf")
+
+    logger.debug("getPublisher called")
+
+    try:
+        chart = config.get("publisher", "chart")
+        interval = config.get("publisher", "interval")
+        server = config.get("publisher", "server")
+        username = config.get("publisher", "username")
+        password = config.get("publisher", "password")
+        remotefolder = config.get("publisher", "remotefolder")
+    except ConfigParser.Error as e:
+        status = 4
+        value = e
+        logger.critical("%s %s", "Unable to get publisher parameters from config", e)
+    else:
+        status = 0
+        value = chart + ',' + str(interval) + ',' + server + ',' + username + ',' + password + ',' + str(remotefolder)
+        logger.debug("%s %s", "returning value ", value)
+
+    status = status + samplerstatus.status()
+
+    return status, value
