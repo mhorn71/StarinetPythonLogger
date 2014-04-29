6b4cbe3b363f68ef6b18df7c986cd7495d678e63
starbase <mhorn71@gmail.com>
2014-04-29 13:31:55 +0100

    Created real actions.capturePublisher command

--- actions/capturePublisher.py
+++ actions/capturePublisher.py
@@ -1,14 +1,111 @@
-__author__ = 'mark'
-
-import utilities.samplerstatus as samplerstatus
+import os.path
+import subprocess
+import utilities.publisherstatus as publisherstatus
 import ConfigParser
+import signal
 import logging
+import sys
+
 
 ##initialise logger
 logger = logging.getLogger('actions.capturePublisher')
 
 config = ConfigParser.RawConfigParser()
-config.read("StarinetBeagleLogger.conf")
+#config.read("StarinetBeagleLogger.conf")
 
-def control(buffero):
-    return 0, None
+
+def control(buffer0):
+
+    status = None
+    value = None
+
+    config.read("StarinetBeagleLogger.conf")
+
+    logger.debug("%s %s", "capturePublisher buffer0 ", buffer0)
+
+    if buffer0 == 'true':
+
+        logger.debug("Entered true routine")
+        if publisherstatus.status() == 1:
+            logger.debug("%s %s", "publisherstatus reports sampler active", str(publisherstatus.status()))
+            status = 8002
+        elif publisherstatus.status() == 0:
+            logger.debug("%s %s", "publisherstatus reports sampler not active", str(publisherstatus.status()))
+            
+            folder = config.get('paths', 'datafolder')
+
+            for the_file in os.listdir(folder):
+                file_path = os.path.join(folder, the_file)
+                try:
+                    if os.path.isfile(file_path):
+                            os.unlink(file_path)
+                    logger.debug("%s %s", "Removing data file ", file_path)
+                except OSError as e:
+                        logger.critical("%s %s", "premature termination", e)
+                        status = 4
+                    
+            f = open(config.get("paths", "datafolder") + '0000', 'wb')
+            f.close()
+            
+            try:
+                pro = subprocess.Popen(["/usr/bin/python", "logger/sampler.py"])
+            except IOError as e:
+                logger.critical("%s %s", "premature termination", e)
+                logger.critical("Unable to start capture")
+                status = 4
+            else:
+                try:
+                    pidfile = open(config.get('paths', 'pidfile'), 'w')
+                    pidfile.write(str(pro.pid))
+                    pidfile.close()
+                except IOError as e:   
+                    logger.critical("%s %s", "premature termination", e)
+                    logger.critical("Unable to create pid file")
+                    status = 4
+                else:
+                    logger.debug("Started logger/sampler ....")
+                    status = 0
+        else:
+            logger.debug("premature termination")
+            status = 4
+
+    elif buffer0 == 'false':
+
+        logger.debug("Entered false routine")
+
+        if publisherstatus.status() == 0:
+            logger.debug("%s %s", "publisherstatus reports sampler not active", str(publisherstatus.status()))
+            status = 0
+        elif publisherstatus.status() == 1:
+            logger.debug("%s %s", "publisherstatus reports sampler active", str(publisherstatus.status()))
+            try:
+                pidfile = open(config.get('paths', 'pidfile'), 'r')
+                pid = int(pidfile.read())
+                pidfile.close()
+                logger.debug("%s %s %s", "logger/sampler pidfile and pid - ", str(pidfile), str(pid))
+            except IOError as e:
+                logger.critical("%s %s", "Unable to assign pid to pro.pid capturePublisher.py", e)
+                status = 4
+            else:
+                try:
+                    os.kill(pid, signal.SIGTERM)
+                except OSError as e:
+                    logger.debug("%s %s", "Unable to kill process publisher/combined", e)
+                    status = 4
+                else:
+                    try:
+                        os.remove(str(config.get('paths', 'pidfile')))
+                    except OSError as e:
+                        logger.critical("%s %s", "Unable to remove pid file fatal error", e)
+                        status = 4
+                    else:
+                        status = 0
+
+    else:
+        logger.critical("invalid parameter")
+        status = 8
+
+    return status, value
+
+if __name__ == "__main__":
+    print control(str(sys.argv[1:]))
