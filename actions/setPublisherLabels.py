21e03a998e2f2a3f22118898e6d8fd22f9f71626
starbase <mhorn71@gmail.com>
2014-05-03 19:47:41 +0100

    Added setPublisherLabels command and config settings.

--- actions/setPublisherLabels.py
+++ actions/setPublisherLabels.py
@@ -1,4 +1,40 @@
 __author__ = 'mark'
+import utilities.publisherstatus as publisherstatus
+import utilities.samplerstatus as samplerstatus
+import ConfigParser
+import logging
+
+##initialise logger
+logger = logging.getLogger('actions.setPublisherLabels')
+
+config = ConfigParser.RawConfigParser()
+config.read("StarinetBeagleLogger.conf")
+
 
 def control(buffer0, buffer1, buffer2, buffer3):
-    return 0, None
+
+    logger.debug("setPublisherLabels called")
+
+    if publisherstatus.status() == 0:
+        status = 2  # ABORT
+        value = 'capturePublisher_ACTIVE'
+    else:
+        try:
+            config.set('publisher', 'channel0', buffer0)  # update
+            config.set('publisher', 'channel1', buffer1)  # update
+            config.set('publisher', 'channel2', buffer2)  # update
+            config.set('publisher', 'channel3', buffer3)  # update
+            with open('StarinetBeagleLogger.conf', 'wb') as configfile:
+                config.write(configfile)
+        except IOError as e:
+            logger.debug("%s %s", "setPublisherLabels IOError ", e)
+            status = 4  # PREMATURE_TERMINATION
+            value = e
+        else:
+            status = 0  # SUCCESS
+            value = None
+    logger.debug("%s %s", "setPublisherLabels returned ", status)
+
+    status = status + samplerstatus.status()
+
+    return status, value
