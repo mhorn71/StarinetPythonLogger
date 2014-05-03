17d70b4e4a62a8e60c43e326ca467ab3a24080d1
starbase <mhorn71@gmail.com>
2014-05-03 20:02:08 +0100

    Created getPublisherLabels.py

--- actions/getPublisherLabels.py
+++ actions/getPublisherLabels.py
@@ -1,5 +1,33 @@
 __author__ = 'mark'
+import utilities.samplerstatus as samplerstatus
+import ConfigParser
+import logging
+
+##initialise logger
+logger = logging.getLogger('actions.getPublisher')
+
+config = ConfigParser.RawConfigParser()
 
 
 def control():
-    return 0, 'channel0 channel1 channel 2 channel 3'
+    config.read("StarinetBeagleLogger.conf")
+
+    logger.debug("getPublisher called")
+
+    try:
+        channel0 = config.get("publisher", "channel0")
+        channel1 = config.get("publisher", "channel1")
+        channel2 = config.get("publisher", "channel2")
+        channel3 = config.get("publisher", "channel3")
+    except ConfigParser.Error as e:
+        status = 4
+        value = e
+        logger.critical("%s %s", "Unable to get publisher parameters from config", e)
+    else:
+        status = 0
+        value = channel0 + ',' + channel1 + ',' + channel2 + ',' + channel3
+        logger.debug("%s %s", "returning value ", value)
+
+    status = status + samplerstatus.status()
+
+    return status, value
