195e6fe8dece10baded26789466606e442e78bb2
Mark <mhorn71@gmail.com>
2015-09-22 21:16:04 +0100

    Updated getPublisherLabels for 7 channels.

--- actions/getPublisherLabels.py
+++ actions/getPublisherLabels.py
@@ -18,13 +18,18 @@ def control():
         channel1 = config.get("publisherlabels", "channel1")
         channel2 = config.get("publisherlabels", "channel2")
         channel3 = config.get("publisherlabels", "channel3")
+        channel4 = config.get("publisherlabels", "channel4")
+        channel5 = config.get("publisherlabels", "channel5")
+        channel6 = config.get("publisherlabels", "channel6")
     except configparser.Error as e:
         status = 4
         value = e
         logger.critical("%s %s", "Unable to get publisher parameters from config", e)
     else:
         status = 0
-        value = channel0 + ',' + channel1 + ',' + channel2 + ',' + channel3
+        value = channel0 + ',' + channel1 + ',' + channel2 + ',' + channel3 + ',' + channel4 + ',' + channel5 + ',' + \
+                channel6
+        
         logger.debug("%s %s", "returning value ", value)
 
     return status, value
