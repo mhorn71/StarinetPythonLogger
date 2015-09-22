f320516ce3e5269bb97e3132f46fc04c0a179813
Mark <mhorn71@gmail.com>
2015-09-22 21:42:37 +0100

    Removed check for publisher running.

--- actions/setPublisherLabels.py
+++ actions/setPublisherLabels.py
@@ -14,29 +14,25 @@ def control(buffer0, buffer1, buffer2, buffer3, buffer4, buffer5, buffer6):
     logger.debug("setPublisherLabels called")
     logger.debug("%s %s %s %s %s %s %s", buffer0, buffer1, buffer2, buffer3, buffer4, buffer5, buffer6)
 
-    if publisherstatus.status() == 0:
-        status = 2  # ABORT
-        value = 'capturePublisher_ACTIVE'
+    try:
+        config.read("StarinetBeagleLogger.conf")
+        config.set('publisherlabels', 'channel0', buffer0)  # update
+        config.set('publisherlabels', 'channel1', buffer1)  # update
+        config.set('publisherlabels', 'channel2', buffer2)  # update
+        config.set('publisherlabels', 'channel3', buffer3)  # update
+        config.set('publisherlabels', 'channel4', buffer4)  # update
+        config.set('publisherlabels', 'channel5', buffer5)  # update
+        config.set('publisherlabels', 'channel6', buffer6)  # update
+        with open('StarinetBeagleLogger.conf', 'w') as configfile:
+            config.write(configfile)
+            configfile.close()
+    except IOError as e:
+        logger.debug("%s %s", "setPublisherLabels IOError ", e)
+        status = 4  # PREMATURE_TERMINATION
+        value = e
     else:
-        try:
-            config.read("StarinetBeagleLogger.conf")
-            config.set('publisherlabels', 'channel0', buffer0)  # update
-            config.set('publisherlabels', 'channel1', buffer1)  # update
-            config.set('publisherlabels', 'channel2', buffer2)  # update
-            config.set('publisherlabels', 'channel3', buffer3)  # update
-            config.set('publisherlabels', 'channel4', buffer4)  # update
-            config.set('publisherlabels', 'channel5', buffer5)  # update
-            config.set('publisherlabels', 'channel6', buffer6)  # update
-            with open('StarinetBeagleLogger.conf', 'w') as configfile:
-                config.write(configfile)
-                configfile.close()
-        except IOError as e:
-            logger.debug("%s %s", "setPublisherLabels IOError ", e)
-            status = 4  # PREMATURE_TERMINATION
-            value = e
-        else:
-            status = 0  # SUCCESS
-            value = None
+        status = 0  # SUCCESS
+        value = None
     logger.debug("%s %s", "setPublisherLabels returned ", status)
 
     return status, value
