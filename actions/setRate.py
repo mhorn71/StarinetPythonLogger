5b569f96f9fea2fdecc95e2ace38f5d195c8316e
Mark <mhorn71@gmail.com>
2015-09-22 21:44:59 +0100

    Removed configparser buffer write parameter

--- actions/setRate.py
+++ actions/setRate.py
@@ -7,29 +7,24 @@ logger = logging.getLogger('actions.setRate')
 config = configparser.RawConfigParser()
 
 
-def control(buffer0, sampler2):
+def control(buffer0):
 
     logger.debug("setRate called")
 
-    if sampler2.status() == 8000:
-        status = 2
-        value = None
+    try:
+        config.read("StarinetBeagleLogger.conf")
+        config.set('capture', 'rate', buffer0)  # update
+        with open('StarinetBeagleLogger.conf', 'w') as configfile:
+            config.write(configfile)
+            configfile.close()
+    except IOError as e:
+        logger.debug("%s %s", "setRate IOError ", e)
+        status = 4
+        value = e
     else:
-        try:
-            config.read("StarinetBeagleLogger.conf")
-            config.set('capture', 'rate', buffer0)  # update
-            with open('StarinetBeagleLogger.conf', 'w') as configfile:
-                config.write(configfile)
-                configfile.close()
-        except IOError as e:
-            logger.debug("%s %s", "setRate IOError ", e)
-            status = 4
-            value = e
-        else:
-            status = 0
-            value = None
-    logger.debug("%s %s", "setRate returned ", status)
+        status = 0
+        value = None
 
-    status = status
+    logger.debug("%s %s %s", "setRate", buffer0, status)
 
     return status, value
