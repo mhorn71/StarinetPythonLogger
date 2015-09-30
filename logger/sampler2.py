935dbf8f3caa7d587d5a8a736f8abd33dc1ff286
Mark <mhorn71@gmail.com>
2015-09-30 20:19:01 +0100

    Added logger messages to sampler2.

--- logger/sampler2.py
+++ logger/sampler2.py
@@ -9,8 +9,6 @@ import logging
 import analogue.readadc as adc
 import logger.temperature as temperature
 
-logger = logging.getLogger('logger.sampler')
-
 
 class Logger:
     def __init__(self, data_array):
@@ -26,6 +24,7 @@ class Logger:
         self.data = data_array
         self.string = ''
         self.block_count = 0
+        self.logger = logging.getLogger('logger.sampler')
 
         thread = threading.Thread(target=self.run)
         thread.daemon = True
@@ -37,9 +36,12 @@ class Logger:
                 self.next_call += self.rate
                 if len(self.string) == 512:
                     self.datafile_write()
+                    self.logger.debug('Appending data to internal memory string.')
                     self.data.append(self.string)
                     self.block_count += 1
+                    self.logger.debug('Next data block integer is : ' + str(self.block_count))
                     if self.block_count == 4096:
+                        self.logger.debug('Data block count has reached 4096 stopping sampler.')
                         self.status_.set()
                     else:
                         stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -54,28 +56,38 @@ class Logger:
 
     def datafile_write(self):
         datafile = hex(self.block_count).split('x')[1].upper().zfill(4)
-        file = open(self.datafolder + datafile, 'wt', encoding='utf-8')
-        file.write(self.string)
-        file.close()
+        self.logger.debug('Writing new data block : ' + str(datafile))
+        try:
+            file = open(self.datafolder + datafile, 'wt', encoding='utf-8')
+            file.write(self.string)
+            file.close()
+        except IOError as msg:
+            self.status_.set()
+            self.logger.critical('Unable to write data block :' + str(datafile) + ' : ' + str(msg))
 
     def start(self):
+        self.logger.debug('Clearing internal data array')
         del self.data[:]
+        self.logger.debug('Data array length :' + str(len(self.data)))
         self.config.read("StarinetBeagleLogger.conf")
         self.rate_string = self.config.get('capture', 'rate')
         self.rate = int(self.rate_string)
+        self.logger.debug('Sampler rate set too : ' + str(self.rate))
         self.block_count = 0
+        self.logger.debug('Block count reset to : ' + str(self.block_count))
         self.next_call = time.time()
         stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.string = str(stamp) + ' ' + temperature.read() + ' ' + self.rate_string + '   ' + adc.read_string()
         self.status_.clear()
-        print('Sampler Started...')
+        self.logger.info('Sampler Started...')
 
     def stop(self):
         self.status_.set()
         if len(self.string) > 0:
+            self.logger.debug('Internal memory string has data flushing to disk.')
             self.data.append(self.string)
             self.datafile_write()
-        print('Sampler Stopped...')
+        self.logger.info('Sampler Stopped...')
 
     def status(self):
 
