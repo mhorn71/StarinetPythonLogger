516b9da325f401cc7fc63d653c7c0c36e60c1310
Mark <mhorn71@gmail.com>
2015-10-07 20:50:26 +0100

    Added GPL to all source files.

--- actions/capture.py
+++ actions/capture.py
@@ -1,3 +1,22 @@
+# StarinetPython3Logger a data logger for the Beaglebone Black.
+# Copyright (C) 2015  Mark Horn
+#
+# This file is part of StarinetPython3Logger.
+#
+# StarinetPython3Logger is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 2 of the License, or (at your option) any
+# later version.
+#
+# StarinetPython3Logger is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with StarinetPython3Logger.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import os.path
 import configparser
 import logging
