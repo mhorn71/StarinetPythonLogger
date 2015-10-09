30cfe9e8f67a0283d07b07d486899704e2973656
Mark <mhorn71@gmail.com>
2015-10-09 15:40:01 +0100

    Updated setup remove ctypes as not needed

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ from cx_Freeze import setup, Executable
 
 base = None
 
-includes = ["crcmod","ctypes"]
+includes = ["crcmod"]
 
 setup(
        name = application_title,
-       version = "5.0.3",
+       version = "5.0.4",
        description = "Starinet Python3 6 Channel Data Logger for Beaglebone Black",
        options = {"build_exe" : {"includes" : includes }},
        executables = [Executable(main_python_file, base = base)])
